23b4c548d90c5c9883381883d8468c6306347277
Reinier Melian Massip <rmelian2014@gmail.com>
2018-09-20 14:43:49 +0200

    updated to latest swift4.2

--- Bezier/CubicCurveAlgorithm.swift
+++ Bezier/CubicCurveAlgorithm.swift
@@ -45,73 +45,69 @@ class CubicCurveAlgorithm
             
             secondControlPoints.append(CGPoint(x: P2x, y: P2y))
         } else {
-            firstControlPoints = Array(count: count, repeatedValue: nil)
-            
+			firstControlPoints = Array(repeating: nil, count: count)
+
             var rhsArray = [CGPoint]()
             
             //Array of Coefficients
             var a = [Double]()
             var b = [Double]()
             var c = [Double]()
-            
-            for var i=0; i<count; i++ {
-                
-                var rhsValueX: CGFloat = 0
-                var rhsValueY: CGFloat = 0
-                
-                let P0 = dataPoints[i];
-                let P3 = dataPoints[i+1];
-                
-                if i==0 {
-                    a.append(0)
-                    b.append(2)
-                    c.append(1)
-                    
-                    //rhs for first segment
-                    rhsValueX = P0.x + 2*P3.x;
-                    rhsValueY = P0.y + 2*P3.y;
-                    
-                } else if i == count-1 {
-                    a.append(2)
-                    b.append(7)
-                    c.append(0)
-                    
-                    //rhs for last segment
-                    rhsValueX = 8*P0.x + P3.x;
-                    rhsValueY = 8*P0.y + P3.y;
-                } else {
-                    a.append(1)
-                    b.append(4)
-                    c.append(1)
-
-                    rhsValueX = 4*P0.x + 2*P3.x;
-                    rhsValueY = 4*P0.y + 2*P3.y;
-                }
-                
-                rhsArray.append(CGPoint(x: rhsValueX, y: rhsValueY))
-            }
+			
+			for i in 0..<count {
+				var rhsValueX: CGFloat = 0
+				var rhsValueY: CGFloat = 0
+				
+				let P0 = dataPoints[i];
+				let P3 = dataPoints[i+1];
+				
+				if i==0 {
+					a.append(0)
+					b.append(2)
+					c.append(1)
+					
+					//rhs for first segment
+					rhsValueX = P0.x + 2*P3.x;
+					rhsValueY = P0.y + 2*P3.y;
+					
+				} else if i == count-1 {
+					a.append(2)
+					b.append(7)
+					c.append(0)
+					
+					//rhs for last segment
+					rhsValueX = 8*P0.x + P3.x;
+					rhsValueY = 8*P0.y + P3.y;
+				} else {
+					a.append(1)
+					b.append(4)
+					c.append(1)
+					
+					rhsValueX = 4*P0.x + 2*P3.x;
+					rhsValueY = 4*P0.y + 2*P3.y;
+				}
+				
+				rhsArray.append(CGPoint(x: rhsValueX, y: rhsValueY))
+			}
             
             //Solve Ax=B. Use Tridiagonal matrix algorithm a.k.a Thomas Algorithm
-            
-            for var i=1; i<count; i++ {
-                let rhsValueX = rhsArray[i].x
-                let rhsValueY = rhsArray[i].y
-                
-                let prevRhsValueX = rhsArray[i-1].x
-                let prevRhsValueY = rhsArray[i-1].y
-                
-                let m = a[i]/b[i-1]
-                
-                let b1 = b[i] - m * c[i-1];
-                b[i] = b1
-                
-                let r2x = rhsValueX.f - m * prevRhsValueX.f
-                let r2y = rhsValueY.f - m * prevRhsValueY.f
-                
-                rhsArray[i] = CGPoint(x: r2x, y: r2y)
-                
-            }
-            
+			for i in 1..<count {
+				let rhsValueX = rhsArray[i].x
+				let rhsValueY = rhsArray[i].y
+				
+				let prevRhsValueX = rhsArray[i-1].x
+				let prevRhsValueY = rhsArray[i-1].y
+				
+				let m = a[i]/b[i-1]
+				
+				let b1 = b[i] - m * c[i-1];
+				b[i] = b1
+				
+				let r2x = rhsValueX.f - m * prevRhsValueX.f
+				let r2y = rhsValueY.f - m * prevRhsValueY.f
+				
+				rhsArray[i] = CGPoint(x: r2x, y: r2y)
+			}
             //Get First Control Points
             
             //Last control Point
@@ -119,57 +115,60 @@ class CubicCurveAlgorithm
             let lastControlPointY = rhsArray[count-1].y.f/b[count-1]
             
             firstControlPoints[count-1] = CGPoint(x: lastControlPointX, y: lastControlPointY)
-            
-            for var i=count-2; i>=0; --i {
-                if let nextControlPoint = firstControlPoints[i+1] {
-                    let controlPointX = (rhsArray[i].x.f - c[i] * nextControlPoint.x.f)/b[i]
-                    let controlPointY = (rhsArray[i].y.f - c[i] * nextControlPoint.y.f)/b[i]
-                    
-                    firstControlPoints[i] = CGPoint(x: controlPointX, y: controlPointY)
-                    
-                }
-            }
+			
+			for i in (0 ..< count - 1).reversed() {
+				if let nextControlPoint = firstControlPoints[i+1] {
+					let controlPointX = (rhsArray[i].x.f - c[i] * nextControlPoint.x.f)/b[i]
+					let controlPointY = (rhsArray[i].y.f - c[i] * nextControlPoint.y.f)/b[i]
+					
+					firstControlPoints[i] = CGPoint(x: controlPointX, y: controlPointY)
+					
+				}
+			}
+		
             
             //Compute second Control Points from first
-            
-            for var i=0; i<count; i++ {
-                
-                if i == count-1 {
-                    let P3 = dataPoints[i+1]
-                    
-                    guard let P1 = firstControlPoints[i] else{
-                        continue
-                    }
-                    
-                    let controlPointX = (P3.x + P1.x)/2
-                    let controlPointY = (P3.y + P1.y)/2
-                    
-                    secondControlPoints.append(CGPoint(x: controlPointX, y: controlPointY))
-                    
-                } else {
-                    let P3 = dataPoints[i+1]
-                    
-                    guard let nextP1 = firstControlPoints[i+1] else {
-                        continue
-                    }
-                    
-                    let controlPointX = 2*P3.x - nextP1.x
-                    let controlPointY = 2*P3.y - nextP1.y
-                    
-                    secondControlPoints.append(CGPoint(x: controlPointX, y: controlPointY))
-                }
-            }
+			
+			for i in 0..<count {
+				
+				if i == count-1 {
+					let P3 = dataPoints[i+1]
+					
+					guard let P1 = firstControlPoints[i] else{
+						continue
+					}
+					
+					let controlPointX = (P3.x + P1.x)/2
+					let controlPointY = (P3.y + P1.y)/2
+					
+					secondControlPoints.append(CGPoint(x: controlPointX, y: controlPointY))
+					
+				} else {
+					let P3 = dataPoints[i+1]
+					
+					guard let nextP1 = firstControlPoints[i+1] else {
+						continue
+					}
+					
+					let controlPointX = 2*P3.x - nextP1.x
+					let controlPointY = 2*P3.y - nextP1.y
+					
+					secondControlPoints.append(CGPoint(x: controlPointX, y: controlPointY))
+				}
+				
+			}
+
         }
         
         var controlPoints = [CubicCurveSegment]()
-        
-        for var i=0; i<count; i++ {
-            if let firstControlPoint = firstControlPoints[i],
-                secondControlPoint = secondControlPoints[i] {
-               let segment = CubicCurveSegment(controlPoint1: firstControlPoint, controlPoint2: secondControlPoint)
-                controlPoints.append(segment)
-            }
-        }
+		
+		for i in 0..<count {
+			if let firstControlPoint = firstControlPoints[i],
+				let secondControlPoint = secondControlPoints[i] {
+				let segment = CubicCurveSegment(controlPoint1: firstControlPoint, controlPoint2: secondControlPoint)
+				controlPoints.append(segment)
+			}
+		}
         
         return controlPoints
     }
--- Bezier/ViewController.swift
+++ Bezier/ViewController.swift
@@ -1,68 +1,67 @@
 //
 //  ViewController.swift
-//  Bezier
+//  BezierSwift3
 //
-//  Created by Ramsundar Shandilya on 10/12/15.
-//  Copyright © 2015 Y Media Labs. All rights reserved.
+//  Created by Reinier Melian Massip on 9/20/18.
+//  Copyright © 2018 Reinier Melian Massip. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    
-    @IBOutlet weak var firstBezierView: BezierView!
-    
-    let dataPoints = [252, 220, 101, 2, 101, 220, 252]
-    
-    var xAxisPoints : [Double] {
-        var points = [Double]()
-        for i in 0..<dataPoints.count {
-            let val = (Double(i)/6.0) * CGRectGetWidth(self.firstBezierView.frame).f
-            points.append(val)
-        }
-        
-        return points
-    }
-    
-    var yAxisPoints: [Double] {
-        var points = [Double]()
-        for i in dataPoints {
-            let val = (Double(i)/255) * CGRectGetHeight(self.firstBezierView.frame).f
-            points.append(val)
-        }
-        
-        return points
-    }
-    
-    var graphPoints : [CGPoint] {
-        var points = [CGPoint]()
-        for i in 0..<dataPoints.count {
-            let val = CGPoint(x: self.xAxisPoints[i], y: self.yAxisPoints[i])
-            points.append(val)
-        }
-        
-        return points
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        firstBezierView.dataSource = self
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        
-        self.firstBezierView.layoutSubviews()
-    }
+	@IBOutlet weak var firstBezierView: BezierView!
+	
+	let dataPoints = [252, 220, 101, 2, 101, 220, 252]
+	
+	var xAxisPoints : [Double] {
+		var points = [Double]()
+		for i in 0..<dataPoints.count {
+			let val = (Double(i)/6.0) * Double(self.firstBezierView.bounds.size.width)
+			points.append(val)
+		}
+		
+		return points
+	}
+	
+	var yAxisPoints: [Double] {
+		var points = [Double]()
+		for i in dataPoints {
+			let val = (Double(i)/255) * Double(self.firstBezierView.bounds.size.height)
+			points.append(val)
+		}
+		
+		return points
+	}
+	
+	var graphPoints : [CGPoint] {
+		var points = [CGPoint]()
+		for i in 0..<dataPoints.count {
+			let val = CGPoint(x: self.xAxisPoints[i], y: self.yAxisPoints[i])
+			points.append(val)
+		}
+		
+		return points
+	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		firstBezierView.dataSource = self
+	}
+	
+	override func viewDidLayoutSubviews() {
+		super.viewDidLayoutSubviews()
+		
+		self.firstBezierView.layoutSubviews()
+	}
 }
 
 extension ViewController: BezierViewDataSource {
-    
-    func bezierViewDataPoints(bezierView: BezierView) -> [CGPoint] {
-        
-        return graphPoints
-    }
+	
+	func bezierViewDataPoints(bezierView: BezierView) -> [CGPoint] {
+		
+		return graphPoints
+	}
 }
 
